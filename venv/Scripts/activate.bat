cb292497a083d2092bb0d02fd94144c9d8043aae
raissa-sousa <raissa.sn.dev@gmail.com>
2026-04-13 16:00:07 -0300

    reestruturação do código entre front e backend

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\MATERIAL CURSO DEV FULL STACK\PROJETO_FINAL\PROJETO_FINAL\venv
+set VIRTUAL_ENV=D:\Pastas GitHub\App Wayne\App-Wayne\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
